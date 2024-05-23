a65d54d1cbb9bf89bd35ef122acffa4d5876c18c
Jean Daly <88166536+artistatbl@users.noreply.github.com>
2024-05-23 17:55:18 +0100

    initial push

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+import { ThemeProvider } from "@/providers/theme-provider";
+
+import "./globals.css";
+
+const font = DM_Sans({ subsets: ['latin'], weight: '400' })
+
+export const metadata: Metadata = {
+  title: "NexusFlow",
+  description: "Workflow Automation and Integration Tool",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
